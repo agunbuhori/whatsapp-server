07aa859d22be2c5cab27455de24bb0a39b55cd84
Jeffry Luqman <jeffry.luqman@gmail.com>
2018-11-14 05:43:14 +0700

    add to messages and send to webhook on progress, to do : target, isGroup

--- src/services/whatsapp_callback/whatsapp_callback.hooks.js
+++ src/services/whatsapp_callback/whatsapp_callback.hooks.js
@@ -1,26 +1,63 @@
-// require('dotenv').config();
-// const request = require('request-promise');
+require('dotenv').config();
+const request = require('request-promise');
 
-// function sentToWebhook() {
-//   return function (hook) {
-//     return hook.app.service('v2/webhooks').find({query: {$limit: 0}})
-//       .then(function (result) {
-//         for(var i = 0; i < result.total; i++){
-//           hook.app.service('v2/webhooks').find({query: {$limit: 1, $skip: i}})
-//             .then(function (result) {
-//               request({
-//                 method: 'POST',
-//                 url: result.data[0].url,
-//                 headers: result.data[0].headers,
-//                 body: hook.result,
-//                 json: true
-//               }).then(message => console.log(message));
-//             });
-//         }
-//         return hook;
-//       });
-//   };
-// }
+function saveToMessages() {
+  return function (hook) {
+    if (typeof hook.data.messages != undefined) {
+      var i;
+      for (i = 0; i < hook.data.messages.length; i++) {
+        if (!hook.data.messages[i].fromMe) {
+          hook.app.service('v2/messages').create({
+            service: {
+              id: 'push_notification',
+              name: 'Push notification',
+            },
+            from: {
+              isMe: hook.data.messages[i].fromMe,
+              target: hook.data.messages[i].author,
+              name: hook.data.messages[i].senderName,
+            },
+            to: {
+              isGroup: false,
+              target: process.env.WA_NUMBER,
+              name: 'HSI Center',
+            },
+            message: {
+              subject: 'No Subject',
+              body: hook.data.messages[i].body,
+            }
+          })
+          .then(result => sendToWebhooks(result));
+        }
+      }
+    }
+    return hook;
+  };
+}
+
+function sendToWebhooks(result) {
+  return request({
+    method: 'GET',
+    url: process.env.API_GATEWAY_URL + '/v2/webhooks',
+    qs: { type: 'messages', $limit: 50 },
+    headers: { Authorization : process.env.API_GATEWAY_ACCESS_TOKEN },
+  }).then(function(response) {
+    webhooks = JSON.parse(response);
+    if (typeof webhooks.data !== 'undefined' && webhooks.data.length > 0) {
+      var i;
+      for (i = 0; i < webhooks.data.length; i++) {
+        request({
+          method: 'POST',
+          url: webhooks.data[i].url,
+          headers: webhooks.data[i].headers,
+          body: result,
+          json: true,
+        }).then(message => console.log(message));
+        console.log(message.data[i]);
+      }
+    }
+  });
+}
 
 module.exports = {
   before: {
@@ -37,7 +74,9 @@ module.exports = {
     all: [],
     find: [],
     get: [],
-    create: [],
+    create: [
+      saveToMessages(),
+    ],
     update: [],
     patch: [],
     remove: []
