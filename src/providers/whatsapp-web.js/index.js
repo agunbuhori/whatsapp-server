a73febd2e4274d0f9202b69a45172d70328428c1
Jeffry Luqman <jeffry.luqman@gmail.com>
2019-03-28 05:11:00 +0700

    implement whatsapp-web.js on progress

---
+++ src/providers/whatsapp-web.js/index.js
@@ -0,0 +1,13 @@
+'use strict';
+
+module.exports = {
+    Client: require('./src/Client'),
+
+    version: '0.1.1',
+
+    // Structures
+    Chat: require('./src/structures/Chat'),
+    PrivateChat: require('./src/structures/PrivateChat'),
+    GroupChat: require('./src/structures/GroupChat'),
+    Message: require('./src/structures/Message')
+};
